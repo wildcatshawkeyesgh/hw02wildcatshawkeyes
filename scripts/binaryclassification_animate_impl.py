b461151472781f6d7b137bdae9b92536681f366d
wildcatshawkeyesgh <wildcatshawkeyesgh@gmail.com>
2026-02-08 19:49:52 -0600

    e

--- scripts/binaryclassification_animate_impl.py
+++ scripts/binaryclassification_animate_impl.py
@@ -14,17 +14,6 @@ from hw02wildcatshawkeyes import deepl
 from hw02wildcatshawkeyes import animation
 
 losses, W1, W2, W3, W4 = deepl.binary_classification(200, 40000, epochs=5000)
-weights = [W1, W2, W3, W4]
-flat_and_cat_weights = torch.nn.utils.parameters_to_vector(weights)
-number_of_weights = flat_and_cat_weights.numel()
-print(number_of_weights)
-rectangle = torch.reshape(
-    flat_and_cat_weights,
-)
-
-x = animation.animate_weight_heatmap(
-    matrix_stack=W1, dt=0.4, title_str="Weight Heatmap"
-)
 
 
 animation.animate_weight_heatmap(
