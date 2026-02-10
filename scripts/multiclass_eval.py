00562f1bffb800dc490d428b839ff7660942b509
wildcatshawkeyesgh <wildcatshawkeyesgh@gmail.com>
2026-02-09 20:49:38 -0600

    final_commit

--- scripts/multiclass_eval.py
+++ scripts/multiclass_eval.py
@@ -23,22 +23,24 @@ df = pd.read_csv(filename)
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 plt.figure()
-plt.boxplot([df["train_accuracy"], df["test_accuracy"]], labels=["Train", "Test"])
+plt.boxplot([df["train_accuracy"], df["test_accuracy"]], tick_labels=["Train", "Test"])
 plt.title("Accuracy")
 plt.savefig(os.path.join(data_dir, f"{keyword}_accuracy_boxplot_{timestamp}.png"))
 
 plt.figure()
-plt.boxplot([df["train_f1"], df["test_f1"]], labels=["Train", "Test"])
+plt.boxplot([df["train_f1"], df["test_f1"]], tick_labels=["Train", "Test"])
 plt.title("F1 Score")
 plt.savefig(os.path.join(data_dir, f"{keyword}_f1_boxplot_{timestamp}.png"))
 
 
 plt.figure()
-plt.boxplot([df["train_precision"], df["test_precision"]], labels=["Train", "Test"])
+plt.boxplot(
+    [df["train_precision"], df["test_precision"]], tick_labels=["Train", "Test"]
+)
 plt.title("Precision")
 plt.savefig(os.path.join(data_dir, f"{keyword}_precision_boxplot_{timestamp}.png"))
 
 plt.figure()
-plt.boxplot([df["train_recall"], df["test_recall"]], labels=["Train", "Test"])
+plt.boxplot([df["train_recall"], df["test_recall"]], tick_labels=["Train", "Test"])
 plt.title("Recall")
 plt.savefig(os.path.join(data_dir, f"{keyword}_recall_boxplot_{timestamp}.png"))
